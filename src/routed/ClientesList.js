52a02632e1146575f3920990cc5c1f4c6ba09cea
fcintra4 <github4@faustocintra.com.br>
2021-10-22 15:47:26 +0000

    (22/10) Atualização do DataGrid para a versão 5.0.0-beta.0

--- src/routed/ClientesList.js
+++ src/routed/ClientesList.js
@@ -10,13 +10,13 @@ import { Toolbar, Button } from '@mui/material'
 
 const useStyles = makeStyles(theme => ({
   dataGrid: {
-    color: theme.palette.text.primary + ' !important',
+    // color: theme.palette.text.primary + ' !important',
     '& .MuiTablePagination-root': {
       color: theme.palette.text.primary
-    },
+    },/*
     '& .MuiIconButton-root': {
       color: theme.palette.text.primary + ' !important'
-    },
+    },*/
     '& .MuiDataGrid-row button': {
       visibility: 'hidden'
     },
